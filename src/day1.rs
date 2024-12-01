f68f060ca41039bdf06d95d7cc3e04db6958048a
Welf Walter <welf.walter@gmail.com>
2024-12-01 22:49:31 +0100

    calculate_total_distance

--- src/day1.rs
+++ src/day1.rs
@@ -2,6 +2,10 @@ use std::vec;
 
 type LocationID = u32;
 
+//////////////////////////////////////////
+/// Input parsing
+//////////////////////////////////////////
+
 fn read_input<'a>(lines:impl Iterator<Item=&'a str>) -> (Vec<LocationID>, Vec<LocationID>) {
     let mut left = Vec::new();
     let mut right = Vec::new();
@@ -17,7 +21,7 @@ fn read_input<'a>(lines:impl Iterator<Item=&'a str>) -> (Vec<LocationID>, Vec<Lo
 #[test]
 fn test_read_input()
 {
-    let input1 = 
+    let input1 =
 "3   4
 4   3
 2   5
@@ -29,6 +33,41 @@ fn test_read_input()
     assert_eq!(right, vec![4,3,5,3,9,3]);
 }
 
+//////////////////////////////////////////
+/// Distance
+//////////////////////////////////////////
+
+fn calculate_total_distance(left_in:&Vec<LocationID>, right_in:&Vec<LocationID>) -> LocationID {
+    let mut left_sorted = left_in.clone();
+    left_sorted.sort();
+    let mut right_sorted = right_in.clone();
+    right_sorted.sort();
+    let mut total_distance = 0;
+    let mut liter = left_sorted.iter();
+    let mut riter = right_sorted.iter();
+    loop {
+        let l = liter.next();
+        let r = riter.next();
+        match (l,r) {
+            (Some(left_value), Some(right_value)) => total_distance += right_value - left_value,
+            (None, None) => return total_distance,
+            (_, _) => panic!("Unexpected ({:?},{:?})", l,r)
+        }
+    }
+}
+
+#[test]
+fn test_distance() {
+    let input1 =
+"3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+    let (left, right) = read_input(input1.split("\n"));
+    assert_eq!(calculate_total_distance(&left, &right), 11);
+}
 
 //////////////////////////////////////////
 /// Puzzle
