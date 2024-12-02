1e4e7918f3d23569ed3d43cecbfe77785a187d65
Welf Walter <welf.walter@gmail.com>
2024-12-02 22:32:18 +0100

    Report::is_safe()

--- src/day2.rs
+++ src/day2.rs
@@ -12,6 +12,59 @@ struct Report {
 
 type Reports = Vec<Report>;
 
+impl Report {
+
+    fn is_slowly_increasing(&self) -> bool {
+        let mut iter = self.levels.iter();
+        let mut last = iter.next().unwrap();
+        for next in iter {
+            if next <= last { return false; }
+            if next - last > 3 { return false; }
+            last = next;
+        }
+        true
+    }
+
+    fn is_slowly_decreasing(&self) -> bool {
+        let mut iter = self.levels.iter();
+        let mut last = iter.next().unwrap();
+        for next in iter {
+            if next >= last { return false; }
+            if last - next > 3 { return false; }
+            last = next;
+        }
+        true
+    }
+
+    fn is_safe(&self) -> bool {
+        self.is_slowly_increasing()
+        ||
+        self.is_slowly_decreasing()
+    }
+}
+
+#[test]
+fn test_is_safe() {
+    let input1 =
+"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+    let reports = parse_reports(input1.split("\n"));
+    assert_eq!(reports[0].is_safe(), true);
+    assert_eq!(reports[1].is_safe(), false);
+    assert_eq!(reports[2].is_safe(), false);
+    assert_eq!(reports[3].is_safe(), false);
+    assert_eq!(reports[4].is_safe(), false);
+    assert_eq!(reports[5].is_safe(), true);
+}
+
+//////////////////////////////////////////
+/// Parsing
+//////////////////////////////////////////
+
 fn parse_report(line:&str) -> Report {
     let mut levels:Vec<Level> = Vec::new();
     for level_str in line.split(" ") {
