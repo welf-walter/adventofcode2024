b768ebff073219b93f72de5b9a0ce1a62d17cc08
Welf Walter <welf.walter@gmail.com>
2024-12-06 22:37:29 +0100

    walk()

--- src/day6.rs
+++ src/day6.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 type Position = (/* x: */usize,/* y: */usize);
 
+#[derive(Debug,Copy,Clone)]
 enum Direction {
     UP,
     RIGHT,
@@ -9,11 +10,36 @@ enum Direction {
     LEFT
 }
 
+impl Direction {
+    fn turn_right(&self) -> Direction {
+        match self {
+            UP    => RIGHT,
+            RIGHT => DOWN,
+            DOWN  => LEFT,
+            LEFT  => UP
+        }
+    }
+}
+
+use Direction::*;
+
 struct Map {
     width:usize,
     height:usize,
     obstructions:HashSet<Position>,
     start:Position // Direction is up
+}
+
+impl Map {
+    // return None if out of area
+    fn step(&self, pos:Position, direction:Direction) -> Option<Position> {
+        match direction {
+            UP    => { if pos.1 > 0             { return Some((pos.0  ,pos.1-1)); } else { return None; }},
+            RIGHT => { if pos.0 < self.width-1  { return Some((pos.0+1,pos.1  )); } else { return None; }},
+            DOWN  => { if pos.1 < self.height-1 { return Some((pos.0  ,pos.1+1)); } else { return None; }},
+            LEFT  => { if pos.0 > 0             { return Some((pos.0-1,pos.1  )); } else { return None; }}
+        }
+    }
 }
 
 fn read_map(lines:Vec<String>) -> Map {
@@ -53,7 +79,7 @@ fn input1() -> Vec<String> {
 }
 
 #[test]
-fn test_map() 
+fn test_map()
 {
     let map = read_map(input1());
     assert_eq!(map.width, 10);
@@ -61,4 +87,35 @@ fn test_map()
     assert_eq!(map.start, (4,6));
     assert_eq!(map.obstructions.len(), 8);
     assert!(map.obstructions.contains(&(2,3)));
+    assert_eq!(map.step((3,4),UP),Some((3,3)));
+    assert_eq!(map.step((3,0),UP),None);
+}
+
+fn walk(map:&Map) -> HashSet<Position> {
+    let mut pos = map.start;
+    let mut direction = Direction::UP;
+    let mut positions:HashSet<Position> = HashSet::new();
+    loop {
+        positions.insert(pos);
+        let new_poso = map.step(pos, direction);
+        if new_poso.is_none() {
+            return positions;
+        }
+        let new_pos = new_poso.unwrap();
+        if map.obstructions.contains(&new_pos) {
+            direction = direction.turn_right();
+        }
+        else
+        {
+            pos = new_pos;
+        }
+    }
+}
+
+#[test]
+fn test_walk() {
+    let map = read_map(input1());
+    let positions = walk(&map);
+    assert!(positions.contains(&(2,4)));
+    assert_eq!(positions.len(), 41);
 }
