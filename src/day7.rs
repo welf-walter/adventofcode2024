5c2366a81c35d924c3fd1b2edcb7fddde68668e1
Welf Walter <welf.walter@gmail.com>
2024-12-07 10:44:32 +0100

    generalize to support any possible_operators list

--- src/day7.rs
+++ src/day7.rs
@@ -4,6 +4,7 @@ type Value = u64;
 enum Operator {
     PLUS,
     MULTIPLY,
+    CONCAT,
     UNKNOWN
 }
 
@@ -33,6 +34,16 @@ fn parse_equation(line:&str) -> Equation {
     Equation { result, operands, operators }
 }
 
+fn concat(a:Value, b:Value) -> Value {
+    format!("{}{}", a, b).parse::<Value>().unwrap()
+}
+
+#[test]
+fn test_concat() {
+    assert_eq!(concat(12,345),12345);
+    assert_eq!(concat(15,6),156);
+}
+
 fn equation_is_true(eq:&Equation) -> bool {
     let mut operand_iter = eq.operands.iter();
     let mut operator_iter = eq.operators.iter();
@@ -45,22 +56,23 @@ fn equation_is_true(eq:&Equation) -> bool {
         myresult = match operator {
             Operator::PLUS => myresult + operand,
             Operator::MULTIPLY => myresult * operand,
+            Operator::CONCAT => concat(myresult, *operand),
             _ => unreachable!()
         }
     }
 }
 
 
-fn equation_can_be_made_true(eq:&Equation) -> bool {
+fn equation_can_be_made_true(eq:&Equation, possible_operators:&Vec<Operator>) -> bool {
     for i in 0..eq.operators.len() {
         if eq.operators[i] == Operator::UNKNOWN {
-            let mut eq1 = eq.clone();
-            eq1.operators[i] = Operator::PLUS;
-            if equation_can_be_made_true(&eq1) { return true; }
-            let mut eq2 = eq.clone();
-            eq2.operators[i] = Operator::MULTIPLY;
-            if equation_can_be_made_true(&eq2) { return true; }
+            for new_operator in possible_operators {
+                let mut eq1 = eq.clone();
+                eq1.operators[i] = *new_operator;
+                if equation_can_be_made_true(&eq1, possible_operators) { return true; }
+            }
 
+            // no completion did work -> cannot be made true
             return false;
         }
     }
@@ -88,19 +100,30 @@ fn test_equation() {
     eq4.operators = vec![Operator::MULTIPLY, Operator::PLUS];
     assert_eq!(equation_is_true(&eq4), true);
 
-    assert_eq!(equation_can_be_made_true(&eq), true);
+    let part1 = vec![Operator::PLUS, Operator::MULTIPLY];
+    assert_eq!(equation_can_be_made_true(&eq, &part1), true);
 
     let eq5 = parse_equation("161011: 16 10 13");
-    assert_eq!(equation_can_be_made_true(&eq5), false);
+    assert_eq!(equation_can_be_made_true(&eq5, &part1), false);
+
+    let part2 = vec![Operator::PLUS, Operator::MULTIPLY, Operator::CONCAT];
+    let eq6 = parse_equation("156: 15 6");
+    assert_eq!(equation_can_be_made_true(&eq6, &part1), false);
+    assert_eq!(equation_can_be_made_true(&eq6, &part2), true);
+    let eq7 = parse_equation("7290: 6 8 6 15");
+    assert_eq!(equation_can_be_made_true(&eq7, &part1), false);
+    assert_eq!(equation_can_be_made_true(&eq7, &part2), true);
+    let eq8 = parse_equation("192: 17 8 14");
+    assert_eq!(equation_can_be_made_true(&eq8, &part2), true);
 
 }
 
-fn count_equations_that_can_be_made_true(eqs:&Equations) -> usize {
-    eqs.iter().map( |eq| if equation_can_be_made_true(eq) { 1 } else { 0 }).sum()
+fn count_equations_that_can_be_made_true(eqs:&Equations, possible_operators:&Vec<Operator>) -> usize {
+    eqs.iter().map( |eq| if equation_can_be_made_true(eq, possible_operators) { 1 } else { 0 }).sum()
 }
 
-fn sum_equations_that_can_be_made_true(eqs:&Equations) -> Value {
-    eqs.iter().map( |eq| if equation_can_be_made_true(eq) {  eq.result } else { 0 }).sum()
+fn sum_equations_that_can_be_made_true(eqs:&Equations, possible_operators:&Vec<Operator>) -> Value {
+    eqs.iter().map( |eq| if equation_can_be_made_true(eq, possible_operators) {  eq.result } else { 0 }).sum()
 }
 
 #[test]
@@ -117,13 +140,19 @@ fn test_equations() {
 292: 11 6 16 20";
     let eqs:Equations = input.split('\n').map(|line| parse_equation(line)).collect();
     assert_eq!(eqs.len(), 9);
-    assert_eq!(equation_can_be_made_true(&eqs[0]), true);
-    assert_eq!(equation_can_be_made_true(&eqs[1]), true);
-    assert_eq!(equation_can_be_made_true(&eqs[2]), false);
+
+    let part1 = vec![Operator::PLUS, Operator::MULTIPLY];
+    assert_eq!(equation_can_be_made_true(&eqs[0], &part1), true);
+    assert_eq!(equation_can_be_made_true(&eqs[1], &part1), true);
+    assert_eq!(equation_can_be_made_true(&eqs[2], &part1), false);
 
     assert_eq!(eqs[1], parse_equation("3267: 81 40 27"));
-    assert_eq!(count_equations_that_can_be_made_true(&eqs), 3);
-    assert_eq!(sum_equations_that_can_be_made_true(&eqs), 3749);
+    assert_eq!(count_equations_that_can_be_made_true(&eqs, &part1), 3);
+    assert_eq!(sum_equations_that_can_be_made_true(&eqs, &part1), 3749);
+
+    let part2 = vec![Operator::PLUS, Operator::MULTIPLY, Operator::CONCAT];
+    assert_eq!(count_equations_that_can_be_made_true(&eqs, &part2), 6);
+    assert_eq!(sum_equations_that_can_be_made_true(&eqs, &part2), 11387);
 }
 
 //////////////////////////////////////////
@@ -142,8 +171,10 @@ pub fn puzzle() {
     let lines:Vec<String> = reader.lines().map( |line| line.unwrap() ).collect();
 
     let eqs:Equations = lines.iter().map(|line| parse_equation(line)).collect();
-    let count = count_equations_that_can_be_made_true(&eqs);
-    let sum = sum_equations_that_can_be_made_true(&eqs);
-    println!("Day 7, Part 1: {} of {} equations can be made true, their sum is {}", count, eqs.len(), sum);
+
+    let part1 = vec![Operator::PLUS, Operator::MULTIPLY];
+    let count1 = count_equations_that_can_be_made_true(&eqs, &part1);
+    let sum1 = sum_equations_that_can_be_made_true(&eqs, &part1);
+    println!("Day 7, Part 1: {} of {} equations can be made true, their sum is {}", count1, eqs.len(), sum1);
 
 }
