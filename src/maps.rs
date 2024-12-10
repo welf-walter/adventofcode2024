beb67b6b9cbc0b3a0abc032a1a85bf25e6bf611b
Welf Walter <welf.walter@gmail.com>
2024-12-10 21:47:08 +0100

    maps::PixelMap<E>

--- src/maps.rs
+++ src/maps.rs
@@ -44,6 +44,7 @@ fn test_direction() {
 //////////////////////////////////////////
 
 // some bounded area to move in
+#[derive(PartialEq, Debug)]
 pub struct Area {
     pub width:usize,
     pub height:usize
@@ -62,4 +63,80 @@ impl Area {
             LEFT  => { if pos.0 > 0             { return Some((pos.0-1,pos.1  )); } else { return None; }}
         }
     }
+}
+
+//////////////////////////////////////////
+/// EnumMap
+//////////////////////////////////////////
+
+pub trait CharBijection {
+    fn from_char(c:char) -> Self;
+    fn to_char(&self) -> char;
+}
+
+pub struct PixelMap<E:CharBijection> {
+    area:Area,
+    pixels:Vec<Vec<E>>
+}
+
+impl<E:CharBijection> PixelMap<E> {
+    fn from_strings<'a>(lines:impl Iterator<Item=&'a str>) -> Self {
+        let mut width = 0;
+        let mut height = 0;
+        let mut pixels = Vec::new();
+        for line in lines {
+            width = 0;
+            let mut row = Vec::new();
+            for c in line.chars() {
+                row.push(E::from_char(c));
+                width = width + 1;
+            }
+            pixels.push(row);
+            height = height + 1;
+        }
+        Self{area:Area{width:width,height:height}, pixels:pixels}
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum TestEnum {
+    A,
+    B,
+    C
+}
+
+impl CharBijection for TestEnum {
+    fn from_char(c:char) -> Self {
+        match c {
+            'A' => TestEnum::A,
+            'B' => TestEnum::B,
+            'C' => TestEnum::C,
+            _ => panic!("Unexpected character {} for TestEnum", c)
+        }
+    }
+    fn to_char(&self) -> char {
+        match self {
+            TestEnum::A => 'A',
+            TestEnum::B => 'B',
+            TestEnum::C => 'C'
+        }
+    }
+}
+
+#[test]
+fn test_from_strings() {
+    type TestMap = PixelMap<TestEnum>;
+    let input =
+"ABC
+BCA";
+    let pixel_map = TestMap::from_strings(input.split('\n'));
+    assert_eq!(pixel_map.area, Area{width:3, height:2});
+    assert_eq!(pixel_map.pixels, vec![
+        vec![TestEnum::A, TestEnum::B, TestEnum::C],
+        vec![TestEnum::B, TestEnum::C, TestEnum::A]
+    ]);
+    assert_eq!(pixel_map.pixels.iter().map(|line| line.iter().map( |e| e.to_char()).collect::<Vec<_>>()).collect::<Vec<_>>(), vec![
+        vec!['A', 'B', 'C'],
+        vec!['B', 'C', 'A']
+    ]);
 }
