be09823cc31c073c5af124e2ff3c286b3ae2580c
Welf Walter <welf.walter@gmail.com>
2024-12-12 23:44:10 +0100

    getting nearer to correct. not yet there.

--- src/day12.rs
+++ src/day12.rs
@@ -23,26 +23,31 @@ fn extract_region(map:&PlantMap, start_position:Position, positions_done:&mut Ha
     let mut perimeter = 0;
     while position_backlog.len() > 0 {
         let current_pos = position_backlog.pop().unwrap();
-        assert!(!positions_done.contains(&current_pos));
-        positions_done.insert(current_pos);
+        if positions_done.contains(&current_pos) {
+            if VERBOSE { println!("  ({},{}): been there. done that.", current_pos.0, current_pos.1);}
+            continue;
+        }
 
         let current_plant = map.at(current_pos);
         if VERBOSE { print!("  ({},{}): {} -> ", current_pos.0, current_pos.1, current_plant);}
         if current_plant == plant {
+            positions_done.insert(current_pos);
             area += 1;
             for direction in Direction::all_directions() {
                 match map.area.step(current_pos, direction) {
                     Some(next_pos) => {
                         if !positions_done.contains(&next_pos) {
                             position_backlog.push(next_pos);
+                            if VERBOSE { print!("Put ({},{}) to backlog ", next_pos.0, next_pos.1);}
                         }
                     },
                     None => {
                         perimeter += 1;
-                        if VERBOSE { println!("border");}
+                        if VERBOSE { print!("{:?} = border ", direction);}
                     }
                 }
             }
+            if VERBOSE { println!("");}
         } else {
             perimeter += 1;
             if VERBOSE { println!("perimeter");}
