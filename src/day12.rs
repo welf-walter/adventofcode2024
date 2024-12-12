89f27b905d68ec5d73934b357b9f591b8a836663
Welf Walter <welf.walter@gmail.com>
2024-12-13 00:02:28 +0100

    sum_of_region_fencing_prices(), more examples

--- src/day12.rs
+++ src/day12.rs
@@ -72,21 +72,53 @@ fn extract_regions(map:&PlantMap) -> Vec<Region> {
     regions
 }
 
+fn sum_of_region_fencing_prices(regions:&Vec<Region>) -> u32 {
+    regions.iter().map(|region| region.area * region.perimeter).sum()
+}
+
 #[test]
 fn test_region() {
-    let input =
+    let input1 =
 "AAAA
 BBCD
 BBCC
 EEEC";
-    let map = PlantMap::from_strings(input.split('\n'));
-    assert_eq!(map.at((2,1)), 'C');
-    let regions = extract_regions(&map);
-    assert_eq!(regions, vec![
+    let map1 = PlantMap::from_strings(input1.split('\n'));
+    assert_eq!(map1.at((2,1)), 'C');
+    let regions1 = extract_regions(&map1);
+    assert_eq!(regions1, vec![
         Region{plant:'A', area: 4, perimeter: 10},
         Region{plant:'B', area: 4, perimeter: 8},
         Region{plant:'C', area: 4, perimeter: 10},
         Region{plant:'D', area: 1, perimeter: 4},
         Region{plant:'E', area: 3, perimeter: 8}
     ]);
+    assert_eq!(sum_of_region_fencing_prices(&region1), 140);
+
+    let input2 =
+"OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO";
+    let map2 = PlantMap::from_strings(input2.split('\n'));
+    let regions2 = extract_regions(&map2);
+    assert_eq!(regions2.len(), 5);
+    assert_eq!(sum_of_region_fencing_prices(&region2), 772);
+
+    let input3 =
+"RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE";
+    let map3 = PlantMap::from_strings(input3.split('\n'));
+    let regions3 = extract_regions(&map3);
+    assert_eq!(regions3.len(), 11);
+    assert_eq!(sum_of_region_fencing_prices(&region2), 1930);
 }
