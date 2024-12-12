0b3b7abce9199a941ed58604e48eb165a2264307
Welf Walter <welf.walter@gmail.com>
2024-12-12 23:55:40 +0100

    flood-fill recursion works now :)

--- src/day12.rs
+++ src/day12.rs
@@ -23,35 +23,39 @@ fn extract_region(map:&PlantMap, start_position:Position, positions_done:&mut Ha
     let mut perimeter = 0;
     while position_backlog.len() > 0 {
         let current_pos = position_backlog.pop().unwrap();
+
+        let current_plant = map.at(current_pos);
+        assert_eq!(current_plant, plant);
+        if VERBOSE { print!("  ({},{}): {} -> ", current_pos.0, current_pos.1, current_plant);}
+
         if positions_done.contains(&current_pos) {
             if VERBOSE { println!("  ({},{}): been there. done that.", current_pos.0, current_pos.1);}
             continue;
         }
-
-        let current_plant = map.at(current_pos);
-        if VERBOSE { print!("  ({},{}): {} -> ", current_pos.0, current_pos.1, current_plant);}
-        if current_plant == plant {
-            positions_done.insert(current_pos);
-            area += 1;
-            for direction in Direction::all_directions() {
-                match map.area.step(current_pos, direction) {
-                    Some(next_pos) => {
+        positions_done.insert(current_pos);
+        area += 1;
+        for direction in Direction::all_directions() {
+            match map.area.step(current_pos, direction) {
+                Some(next_pos) => {
+                    let next_plant = map.at(next_pos);
+                    if next_plant == current_plant {
                         if !positions_done.contains(&next_pos) {
                             position_backlog.push(next_pos);
                             if VERBOSE { print!("Put ({},{}) to backlog ", next_pos.0, next_pos.1);}
                         }
-                    },
-                    None => {
+                    } else {
                         perimeter += 1;
-                        if VERBOSE { print!("{:?} = border ", direction);}
+                        if VERBOSE { println!("{:?} = perimeter", direction);}
                     }
+                },
+                None => {
+                    perimeter += 1;
+                    if VERBOSE { print!("{:?} = border ", direction);}
                 }
             }
-            if VERBOSE { println!("");}
-        } else {
-            perimeter += 1;
-            if VERBOSE { println!("perimeter");}
         }
+        if VERBOSE { println!("");}
+
     }
 
     Region { plant, area, perimeter }
