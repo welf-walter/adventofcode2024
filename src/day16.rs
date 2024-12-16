421031c556cbf2ad67c1476eeff6290d5f072b1c
Welf Walter <welf.walter@gmail.com>
2024-12-16 23:39:56 +0100

    add tracing. add backlog to avoid duplicate states

--- src/day16.rs
+++ src/day16.rs
@@ -2,6 +2,8 @@ use crate::maps::Position;
 use crate::maps::Direction;
 use Direction::*;
 
+const VERBOSE:bool = true;
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum MapElement {
     Space,
@@ -48,16 +50,21 @@ fn cost_of_action(action:Action) -> Cost {
 type State = (Position,Direction);
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 struct Puzzle {
     map:Map,
-    cache:HashMap<State,Cost>
+    // already evaluated
+    cache:HashMap<State,Cost>,
+    // currently in evaluation
+    backlog:HashSet<State>
 }
 
 impl Puzzle {
     fn read_input<'a>(map_lines:impl Iterator<Item=&'a str>) -> Puzzle {
         Puzzle {
             map:Map::from_strings(map_lines),
-            cache:HashMap::new()
+            cache:HashMap::new(),
+            backlog:HashSet::new()
         }
     }
 
@@ -92,20 +99,38 @@ impl Puzzle {
     fn get_cost_of_state(&mut self, state:State) -> Cost {
         // ... todo
         if let Some(cached) = self.cache.get(&state) {
+            if VERBOSE { println!("Cached: {:?} -> {}", state, *cached)}
             return *cached;
         }
+
+        // recursion termination
         if self.map.at(state.0) == End {
+            if VERBOSE { println!("Terminated at {:?}", state);}
             return 0;
         }
+
+        assert!(!self.backlog.contains(&state));
+        self.backlog.insert(state);
         // first try to walk, because it is cheaper
         let mut options:Vec<(Action, Cost)> = Vec::new();
         for action in [Walk, TurnRight, TurnLeft] {
-            if let Some(after) = self.execute_action(state, Walk) {
+            if VERBOSE { println!("At {:?} do {:?}", state, action);}
+            if let Some(after) = self.execute_action(state, action) {
+                // don't evaluate if already on stack
+                if self.backlog.contains(&after) {
+                    if VERBOSE { println!("At {:?} do {:?} -> skip", state, action);}
+                    continue;
+                }
                 let cost = cost_of_action(action) + self.get_cost_of_state(after);
+                if VERBOSE { println!("At {:?} do {:?} -> {}", state, action, cost)}
                 options.push((action, cost));
             }
         }
-        options.iter().min_by_key(|(_action,cost)| cost).unwrap().1
+        self.backlog.remove(&state);
+        let best_option = options.iter().min_by_key(|(_action,cost)| cost).unwrap();
+        let best_cost = best_option.1;
+        self.cache.insert(state, best_cost);
+        best_cost
     }
 }
 
