d0891c09f039edb10f465815d0a3b6d5df74798b
Welf Walter <welf.walter@gmail.com>
2025-01-06 22:59:25 +0100

    more unit test

--- src/day11.rs
+++ src/day11.rs
@@ -43,4 +43,24 @@ fn test_change_stones() {
     let input1  = vec![0,    1,   10,   99,     999];
     let expect1 = vec![1, 2024, 1, 0, 9, 9, 2021976];
     assert_eq!(change_stones(input1), expect1);
+
+    let initial = vec![125, 17];
+    let blink1 = change_stones(initial);
+    assert_eq!(blink1, vec![253000, 1, 7]);
+    let blink2 = change_stones(blink1);
+    assert_eq!(blink2, vec![253, 0, 2024, 14168]);
+    let blink3 = change_stones(blink2);
+    assert_eq!(blink3, vec![512072, 1, 20, 24, 28676032]);
+    let blink4 = change_stones(blink3);
+    assert_eq!(blink4, vec![512, 72, 2024, 2, 0, 2, 4, 2867, 6032]);
+    let blink5 = change_stones(blink4);
+    assert_eq!(blink5, vec![1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32]);
+    let blink6 = change_stones(blink5);
+    assert_eq!(blink6, vec![2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2]);
+    assert_eq!(blink6.len(), 22);
+    let mut blinkn = blink6;
+    for _ in 6..25 {
+        blinkn = change_stones(blinkn);
+    }
+    assert_eq!(blinkn.len(), 55312);
 }
