51d6d660c7ae2ac4ee237b38ea35480c35a763b6
Welf Walter <welf.walter@gmail.com>
2025-01-07 22:29:23 +0100

    finish parsing

--- src/day13.rs
+++ src/day13.rs
@@ -4,6 +4,7 @@ type Position = u32;
 const COST_OF_A:Cost = 3;
 const COST_OF_B:Cost = 1;
 
+#[derive(Debug, PartialEq)]
 struct Machine {
     // button A moves (X,Y)
     a:(Position, Position),
@@ -17,10 +18,36 @@ struct Machine {
 
 use pest::Parser;
 use pest_derive::Parser;
+use pest::iterators::Pair;
 
 #[derive(Parser)]
 #[grammar = "../grammar/day13.pest"]
 struct Day13Parser;
+
+
+fn build_machine(machine_rule:Pair<'_, Rule>) -> Machine {
+    let numbers:Vec<Position> = machine_rule.into_inner().map(|pair| pair.as_str().parse::<Position>().unwrap() ).collect();
+    Machine {
+        a:(numbers[0], numbers[1]),
+        b:(numbers[2], numbers[3]),
+        prize:(numbers[4],numbers[5])
+    }
+}
+
+fn build_file(file_rule:Pair<'_, Rule>) -> Vec<Machine> {
+    let mut machines = Vec::new();
+    for pair in file_rule.into_inner() {
+        match pair.as_rule() {
+            Rule::machine => {
+                let machine = build_machine(pair);
+                machines.push(machine);
+            }
+            _ => { println!("Unexpected {}", pair); }
+        }
+    }
+    machines
+}
+
 
 #[cfg(test)]
 fn example1() -> &'static str {
@@ -55,8 +82,12 @@ Prize: X=8400, Y=5400
 ";
 
     assert!(Day13Parser::parse(Rule::machine, input1).is_ok());
+    let machine1 = build_machine(Day13Parser::parse(Rule::machine, input1).unwrap().peek().unwrap());
+    assert_eq!(machine1, Machine{a:(94,34),b:(22,67),prize:(8400,5400)});
 
     assert!(Day13Parser::parse(Rule::file, example1()).is_ok());
+    let machines = build_file(Day13Parser::parse(Rule::file, example1()).unwrap().peek().unwrap());
+    assert_eq!(machines.len(), 4);
 
 }
 
