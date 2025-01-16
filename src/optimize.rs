17b6e3c8bd0ac01a695c7000a238a46e92257714
Welf Walter <welf.walter@gmail.com>
2025-01-16 22:39:12 +0100

    find_all_best_path_to_end_states()

--- src/optimize.rs
+++ src/optimize.rs
@@ -31,13 +31,14 @@ type CostCache<State> = HashMap<State,Cost>;
 struct ProblemSolver<'p, P:Problem> {
     problem:&'p P,
     start_state:P::State,
-    cost_cache:CostCache<P::State>
+    cost_cache:CostCache<P::State>,
+    best_predecessors:HashMap<P::State,Vec<(P::State,P::Action)>>
 }
 
 impl<P:Problem> ProblemSolver<'_, P> {
 
     fn new(problem:&P, start_state:P::State) -> ProblemSolver<P> {
-        ProblemSolver { problem, start_state, cost_cache:CostCache::new() }
+        ProblemSolver { problem, start_state, cost_cache:CostCache::new(), best_predecessors:HashMap::new() }
     }
 
     // from an unordered list of states, extract one with minimal cost
@@ -70,7 +71,9 @@ impl<P:Problem> ProblemSolver<'_, P> {
             for &action in P::Action::all_actions() {
                 if VERBOSE { println!("  try to do {:?}", action);}
                 if let Some(after) = self.problem.execute_action(state, action) {
-                    let cost_this_way = action.cost() + current_cost;
+                    let new_cost = action.cost();
+                    assert!(new_cost > 0);
+                    let cost_this_way = new_cost + current_cost;
 
                     // recursion termination
                     if self.problem.is_end_state(&after) {
@@ -96,6 +99,75 @@ impl<P:Problem> ProblemSolver<'_, P> {
         }
 
         return None;
+    }
+
+    // find all best path and fill predecessor cache
+    fn find_all_best_path_to_end_states(&mut self, maximal_cost:Cost) -> Vec<(/* endstate: */P::State, /* cost: */Cost)> where <P as Problem>::Action: 'static {
+
+        // these states are to investigate
+        let mut backlog:Vec<P::State> = Vec::new();
+        let mut end_states:HashMap<P::State, Cost> = HashMap::new();
+
+        self.cost_cache.insert(self.start_state, 0);
+        backlog.push(self.start_state);
+
+        // recursion termination at start point
+        if self.problem.is_end_state(&self.start_state)  {
+            if VERBOSE { println!("Terminated at start");}
+            return vec![(self.start_state, 0)];
+        }
+
+        while backlog.len() > 0 {
+            // extract element with minimum cost
+            let (state, current_cost) = self.extract_cheapest_state(&mut backlog);
+            if VERBOSE { println!("Handle {:?} with cost = {}", state, current_cost);}
+
+            for &action in P::Action::all_actions() {
+                if VERBOSE { println!("  try to do {:?}", action);}
+                if let Some(after) = self.problem.execute_action(state, action) {
+                    let new_cost = action.cost();
+                    assert!(new_cost > 0);
+                    let cost_this_way = new_cost + current_cost;
+
+                    if cost_this_way > maximal_cost {
+                        if VERBOSE { println!("Ignore because cost {} > max cost {}", cost_this_way, maximal_cost);}
+                        break;
+                    }
+
+                    if let Some(&best_cost_up_to_now) = self.cost_cache.get(&after) {
+                        if cost_this_way < best_cost_up_to_now {
+                            self.cost_cache.insert(after, cost_this_way);
+                            self.best_predecessors.insert(after, vec![(state, action)]);
+                            if VERBOSE { println!("  better cost for {:?}: {} < {}", after, cost_this_way, best_cost_up_to_now)}
+
+                            if self.problem.is_end_state(&after) {
+                                if VERBOSE { println!("Found cheaper way to new end state {:?} with cost {}", after, cost_this_way);}
+                                end_states.insert(after, cost_this_way);
+                            } else {
+                                backlog.push(after);
+                            }
+                        } else if cost_this_way == best_cost_up_to_now {
+                            self.best_predecessors.get_mut(&after).unwrap().push((state, action));
+                            if VERBOSE { println!("  equal cost for {:?}: {}", after, cost_this_way)}
+                            assert!(self.cost_cache.contains_key(&after) || backlog.contains(&after));
+                        }
+                    } else {
+                        self.cost_cache.insert(after, cost_this_way);
+                        self.best_predecessors.insert(after, vec![(state, action)]);
+                        if VERBOSE { println!("  first cost for {:?}: {}", after, cost_this_way)}
+                        if self.problem.is_end_state(&after) {
+                            if VERBOSE { println!("Found new end state {:?} with cost {}", after, cost_this_way);}
+                            end_states.insert(after, cost_this_way);
+                        } else {
+                            backlog.push(after);
+                        }
+                    }
+                }
+            }
+        }
+
+        // convert hash map to vector
+        end_states.iter().map(|(&state,&cost)|(state,cost)).collect::<Vec<(/* endstate: */P::State, /* cost: */Cost)>>()
     }
 }
 
@@ -128,7 +200,7 @@ pub fn get_cost_cache<P:Problem>(problem:&P, start_state:P::State) -> CostCache<
 #[cfg(test)]
 mod test {
 
-use crate::optimize::{get_cost_cache, get_cost_of_state, Problem};
+use crate::optimize::{get_cost_cache, get_cost_of_state, Problem, ProblemSolver};
 
 use super::ActionTrait;
 
@@ -190,6 +262,28 @@ fn test_actions() {
     assert_eq!(cost_cache.get(&TestState{value:2}), Some(&1));
     assert_eq!(cost_cache.get(&TestState{value:3}), Some(&2));
     assert_eq!(cost_cache.get(&TestState{value:4}), Some(&2));
+
+}
+
+#[test]
+fn test_all_paths() {
+    let problem = TestProblem {target_value:5};
+    let mut solver = ProblemSolver::new(&problem, TestState{value:1});
+    let endstates = solver.find_all_best_path_to_end_states(10);
+    assert_eq!(endstates, vec![(TestState{value:5},3)]);
+    assert_eq!(endstates.len(), 1);
+    let (endstate, endcost) = endstates[0];
+    assert_eq!(endstate, TestState{value:5});
+    assert_eq!(endcost, 3);
+ 
+    assert_eq!(solver.cost_cache.get(&TestState{value:2}).unwrap(),&1);
+    assert_eq!(solver.best_predecessors.get(&TestState{value:2}).unwrap(),&vec![(TestState{value:1}, TestAction::Double), (TestState{value:1}, TestAction::Increment)]);
+    assert_eq!(solver.cost_cache.get(&TestState{value:3}).unwrap(),&2);
+    assert_eq!(solver.best_predecessors.get(&TestState{value:3}).unwrap(),&vec![(TestState{value:2}, TestAction::Increment)]);
+    assert_eq!(solver.cost_cache.get(&TestState{value:4}).unwrap(),&2);
+    assert_eq!(solver.best_predecessors.get(&TestState{value:4}).unwrap(),&vec![(TestState{value:2}, TestAction::Double)]);
+    assert_eq!(solver.cost_cache.get(&TestState{value:5}).unwrap(),&3);
+    assert_eq!(solver.best_predecessors.get(&TestState{value:5}).unwrap(),&vec![(TestState{value:4}, TestAction::Increment)]);
 }
 
 }
